20452219e65edb36dde9a21f3e24e08afa8bb072
HaoYongZhang <ioert123great@gmail.com>
2019-03-12 20:00:14 +0800

    加入全局样式

--- src/App.vue
+++ src/App.vue
@@ -7,17 +7,6 @@
     <router-view/>
   </div>
 </template>
-<script>
-    export default {
-    
-    }
-    document.addEventListener('DOMContentLoaded', () => {
-        let fontSize = window.innerWidth / 10
-        fontSize = fontSize > 50 ? 50 : fontSize
-        const html = document.querySelector('html')
-        html.style.fontSize = fontSize + 'px'
-    })
-</script>
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
       ? './'
       : '/'
-  }
+}
 // module.exports = {
 //     publicPaths: process.env.NODE_ENV  === 'production'?'./':'/'
 //   }
