42cf4f769d65abad779d50c8bd810c88dc3ac4a3
HaoYongZhang <ioert123great@gmail.com>
2019-03-12 17:08:11 +0800

    z-book项目初始化

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+      ? './'
+      : '/'
+  }
+// module.exports = {
+//     publicPaths: process.env.NODE_ENV  === 'production'?'./':'/'
+//   }
